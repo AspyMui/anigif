a853b3d179d6eb8aef954dc0561cd135a112a3dd
AspyMui <aspymui@gmail.com>
2021-12-18 13:21:43 -0600

    Init

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin'
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp'
+          }
+        ]
+      }
+    ];
+  }
+};
